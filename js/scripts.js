f8de9991605fb0cf24d14d81449ec27ac1d67503
DanielAdeyemi <adeyemidany@gmail.com>
2021-01-24 22:47:11 -0800

    add AddressBook database for storing contacts and add method to add contact into the book

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+// Bussiness logic for AddressBook
+function AddressBook() {
+  this.contacts = {};
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+}
+
+// Bussiness logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
